53ab2e01aea2b0d3f822192c58d0f7c00ecf2144
CristhianLimaPeru <cristhiangroupinnova@gmail.com>
2024-10-18 05:28:36 -0500

    First commit microservice

---
+++ orders-ms/src/config/envs.ts
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import * as joi from 'joi';
+
+interface EnvVars {
+    PORT: number;
+}
+
+const envSchema = joi
+    .object({
+        PORT: joi.number().required(),
+        PRODUCTS_MICROSERVICE_HOST: joi.string().required(),
+        PRODUCTS_MICROSERVICE_PORT: joi.number().required(),
+    })
+    .unknown(true);
+
+const { error, value } = envSchema.validate(process.env);
+
+if (error) {
+    throw new Error(`Config validation error: ${error.message}`);
+}
+
+const envVar: EnvVars = value;
+
+export const envs = {
+    port: envVar.PORT,
+};
